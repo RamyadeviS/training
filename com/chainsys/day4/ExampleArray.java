0d361cbcea104408f6fb5296d31d8e0ce19f988e
Ramyadevi S <ramy3344@global.chainsys.com>
2022-11-16 17:40:56 +0530

    files updated

---
+++ com/chainsys/day4/ExampleArray.java
@@ -0,0 +1,11 @@
+package com.chainsys.day4;
+
+public class ExampleArray {
+	public static void main(String args[]) {
+		int[] marks = { 34, 1, 3, 2, 4, 6, 7 };
+		for (int i = 0; i < marks.length; i++) {
+			System.out.println(marks[i]);
+		}
+
+	}
+}
